85f082879831782ec54268dd9c20e1f8ebefc0fc
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-05-30 10:03:44 +0300

    PersistGate disabled

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // Використовуємо локальне сховище
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
-// Конфігурація для збереження поля items зі слайса контактів
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  whitelist: ['items'], // Зберігаємо тільки поле items
-};
-
-// Застосовуємо конфігурацію до редюсера слайса контактів
-const persistedContactsReducer = persistReducer(contactsPersistConfig, contactsReducer);
 
 const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactsReducer, 
     filters: filtersReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+        ignoredActions: [],
       },
     }),
 });
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export {store}
